5da0a227ba74b83f1f95ab4ddea35d58b83a4193
Yuvraj <yuvrajsharma21314@gmail.com>
2026-05-13 16:34:56 +0530

    Pop method

--- Linked-LIst.js
+++ Linked-LIst.js
@@ -10,7 +10,7 @@ class linkedList {
   constructor(value) {
     this.head = new Node(value);
     this.tail = this.head;
-    this.lenght = 1;
+    this.length = 1;
   }
   push(value) {
     let newNode = new Node(value);
@@ -21,10 +21,36 @@ class linkedList {
     }
     this.tail.next = newNode;
     this.tail = newNode;
-    this.lenght++;
+    this.length++;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let temp = this.head;
+    let prev = this.head;
+
+    while (temp.next) {
+      prev = temp;
+      temp = prev.next;
+    }
+
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
   }
 }
 
 const myLinkedList = new linkedList(1);
 myLinkedList.push(10);
+myLinkedList.pop();
 console.log(myLinkedList);
